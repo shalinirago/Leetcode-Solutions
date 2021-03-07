a9d8122bd15828015e2ef2112de062a6ec8af1f4
shalinirago <49813882+shalinirago@users.noreply.github.com>
2021-03-07 12:21:52 -0500

    Time: 4 ms (49.91%), Space: 8.3 MB (49.99%) - LeetHub

---
+++ sort-colors/sort-colors.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i, key, j;
+        
+        for (int i = 1; i < nums.size(); i++)
+        {
+            key = nums[i];
+            j = i-1;
+            
+            while (j >= 0 && nums[j] > key)
+            {
+                nums[j+1] = nums[j];
+                j = j-1;
+            }
+            nums[j+1] = key;
+        }
+    }
+};
